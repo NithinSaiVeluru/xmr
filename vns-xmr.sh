31536b750edcb022dcbde46eac229868d5db102c
vns1111 <veluruns@gmail.com>
2022-02-25 22:38:59 -0700

    Update vns-xmr.sh

--- vns-xmr.sh
+++ vns-xmr.sh
@@ -9,4 +9,4 @@ screen -R xmr
 wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-focal-x64.tar.gz
 tar xvzf xmrig-6.16.4-focal-x64.tar.gz
 cd xmrig-6.16.4
-./xmrig -o us-west.minexmr.com:443 -u 8BzzDzMwc4GQvohjahR2sYgzFfNDugPCHJf94Lqrzj4WDxCeviY1eVuRZKBzrXsFKMUEeGDg9t3WhDEj7FuNYUdJULc42zw -k --tls --rig-id aank
+./xmrig -o us-west.minexmr.com:443 -u 8BzzDzMwc4GQvohjahR2sYgzFfNDugPCHJf94Lqrzj4WDxCeviY1eVuRZKBzrXsFKMUEeGDg9t3WhDEj7FuNYUdJULc42zw -k --tls --rig-id vns
