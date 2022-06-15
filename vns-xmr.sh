3e59029315b357bf55b7e8238f0a3e9da648fa42
vns1111 <veluruns@gmail.com>
2022-06-15 07:45:38 -0700

    Update vns-xmr.sh

--- vns-xmr.sh
+++ vns-xmr.sh
@@ -7,4 +7,4 @@ sudo apt install screen
 wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-focal-x64.tar.gz
 tar xvzf xmrig-6.16.4-focal-x64.tar.gz
 cd xmrig-6.16.4
-./xmrig -o sg.minexmr.com:443 -u 8BzzDzMwc4GQvohjahR2sYgzFfNDugPCHJf94Lqrzj4WDxCeviY1eVuRZKBzrXsFKMUEeGDg9t3WhDEj7FuNYUdJULc42zw -k --tls --rig-id vns
+./xmrig -o xmr-asia1.nanopool.org:14433 -u 8BzzDzMwc4GQvohjahR2sYgzFfNDugPCHJf94Lqrzj4WDxCeviY1eVuRZKBzrXsFKMUEeGDg9t3WhDEj7FuNYUdJULc42zw -k --tls --rig-id vns
